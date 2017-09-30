77cc6d0fe93ee8ea2210543e5a2bb50a896d4a65
Rosho123 <rsf.sho@gmail.com>
2017-09-29 20:08:11 -0700

    initial

---
+++ index.js
@@ -0,0 +1,41 @@
+var app = require('http').createServer()
+var io = require('socket.io')(app)
+var fs = require('fs')
+
+var canidates = []
+var db = {}
+fs.readFile(__dirname + '/config.json', function (err, data) {
+  if (err) {
+    throw err
+  }
+  data = JSON.parse(data.toString())
+  canidates = data.canidates
+  db = data
+})
+app.listen(8000)
+
+io.on('connection', function (socket) {
+  console.log('connection')
+  socket.on('vote', function (canidate, uid, email, name) {
+    console.log(canidate, uid, email, name)
+    console.log(db.voting)
+    db.voting[canidate.toString()].votes.push(uid)
+    db.voting.users.uid = true
+  })
+  socket.on('checkIfVoted', function (uid) {
+    console.log('checking', uid)
+    if (db.voting.users.uid) {
+      socket.emit('allreadyvoted')
+      console.log('allreadyvoted')
+    } else {
+      socket.emit('checksOut')
+      console.log('checksout')
+    }
+  })
+  socket.on('getCanidates', function () {
+    socket.emit('canidates', canidates)
+  })
+  socket.on('disconnect', function () {
+    console.log('user disconnected')
+  })
+})
